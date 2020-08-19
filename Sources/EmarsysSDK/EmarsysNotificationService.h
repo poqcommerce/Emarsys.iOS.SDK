6506fea026225c9b28850a90d475d261d7c48185
Joshua White <joshua.white@poqcommerce.com>
2020-08-19 16:14:14 +0100

    Expose the EmarsysNotificationService header to the public for import

---
+++ Sources/EmarsysSDK/EmarsysNotificationService.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double CoreSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char CoreSDKVersionString[];
+
+#import <EmarsysNotificationService/EMSNotificationService.h>
